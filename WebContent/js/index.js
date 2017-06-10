2c05bdf6b2c325126964c8ed5d5e0fade3d35a72
chenxing <1285258619@qq.com>
2017-06-10 15:58:02 +0800

    重构了之前的代码，添加键盘监听

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -3,9 +3,11 @@
  */
 var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor : 0x1099bb});
 document.body.appendChild(app.view);
+//游戏标题
 var basicText = new PIXI.Text('2048game',{
 	fontSize:50
 });
+//锚点设置在中间,方便定位
 basicText.anchor.set(0.5);
 basicText.x = app.renderer.width / 2;
 basicText.y = app.renderer.height / 4;
@@ -16,33 +18,48 @@ var grid = [];
 for(var i = 0; i < 4; i ++){
 	grid[i] = [0,0,0,0];
 }
+//生成随机的x,y坐标
+var x = generateRandomNumber();
+var y = generateRandomNumber();
+grid[x][y] = 2;
 //画4*4方格
 for(var i = 0; i < 4; i++){
 	for(var j = 0; j < 4; j++){
-		var graphics = new PIXI.Graphics();
-		graphics.beginFill(0xFFCC99, 1);
-		graphics.drawRect(app.renderer.width / 8 + j * 77, app.renderer.height / 8 * 3 + i * 77, 75, 75);
-		app.stage.addChild(graphics);
+		drawCells(i, j);
 	}
 }
 //生成随机数
 function generateRandomNumber(){
 	return Math.floor(Math.random() * 4);
 }
-//生成随机的x,y坐标
-var x = generateRandomNumber();
-var y = generateRandomNumber();
-//画出随机生成坐标上的方块
-var graphics = new PIXI.Graphics();
-graphics.beginFill(0xFFCC66, 1);
-graphics.drawRect(app.renderer.width / 8 + y * 77, app.renderer.height / 8 * 3 + x * 77, 75, 75);
-app.stage.addChild(graphics);
-//画出随机生成坐标上的数字
-var basicText = new PIXI.Text('2',{
-	fontSize:50
-});
-basicText.anchor.set(0.5);
-basicText.x = 75/2 + app.renderer.width / 8 + y * 77;
-basicText.y = 75/2 + app.renderer.height / 8 * 3 + x * 77;
 
-app.stage.addChild(basicText);
+//绘画游戏窗体
+function drawCells(x, y){
+	var color = 0xFFCC66;
+	if(grid[x][y] === 2){
+		color = 0xFF9933;
+	}
+	//画出坐标上的方块
+	var graphics = new PIXI.Graphics();
+	graphics.beginFill(color, 1);
+	graphics.drawRect(app.renderer.width / 8 + y * 77, app.renderer.height / 8 * 3 + x * 77, 75, 75);
+	app.stage.addChild(graphics);
+	if(grid[x][y] != 0){
+		//画出坐标上的数字
+		var basicText = new PIXI.Text(grid[x][y],{
+			fontSize:50
+		});
+		basicText.anchor.set(0.5);
+		basicText.x = 75/2 + app.renderer.width / 8 + y * 77;
+		basicText.y = 75/2 + app.renderer.height / 8 * 3 + x * 77;
+
+		app.stage.addChild(basicText);
+	}
+}
+//添加键盘监听事件
+document.addEventListener('keydown',function(event){
+	if(event.code === 'ArrowLeft'){
+		console.log(event);
+	}
+	
+});
